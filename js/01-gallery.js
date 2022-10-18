67d4f5c598afd032ad1eb8db6a815f0c66641036
Svitlana Tymoshevska <amelia.tsa@gmail.com>
2022-10-18 21:57:37 +0300

    Basic execution of task 1.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryParentEl = document.querySelector('.gallery');
+
+const galleryEls = galleryItems.map(makeGalleryItem).join('');
+
+function makeGalleryItem({ preview, original, description }) {
+    const itemEl = `<div class="gallery__item">
+                        <a class="gallery__link" href=${original}>
+                            <img
+                            class="gallery__image"
+                            src=${preview}
+                            data-source=${original}
+                            alt=${description}
+                            />
+                        </a>
+                    </div>`;
+    return itemEl;
+}
+
+function onPreviewImgClick(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    
+    openOriginalImgModally(event);
+}
+
+function openOriginalImgModally(event) {
+    event.preventDefault();
+
+    const instance = basicLightbox.create(`
+        <div class="modal">
+            <img src='${event.target.dataset.source}'/>
+        </div>
+    `);
+    instance.show();
+}
+
+galleryParentEl.insertAdjacentHTML('beforeend', galleryEls);
+
+galleryParentEl.addEventListener('click', onPreviewImgClick);
