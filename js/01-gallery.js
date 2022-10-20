3750316aa68b14e7894cb6dde133757238d85c40
Svitlana Tymoshevska <amelia.tsa@gmail.com>
2022-10-20 17:37:22 +0300

    Completed task 7.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,55 +1,67 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const galleryParentEl = document.querySelector('.gallery');
 
-const galleryEls = galleryItems.map(makeGalleryItem).join('');
-
-galleryParentEl.insertAdjacentHTML('beforeend', galleryEls);
-
-galleryParentEl.addEventListener('click', onPreviewImgClick);
-
-function makeGalleryItem({ preview, original, description }) {
-    const itemEl = `<div class="gallery__item">
-                        <a class="gallery__link" href=${original}>
-                            <img
-                            class="gallery__image"
-                            src=${preview}
-                            data-source=${original}
-                            alt=${description}
-                            />
-                        </a>
-                    </div>`;
-    return itemEl;
-}
-
-function onPreviewImgClick(event) {
-    if (event.target.nodeName !== 'IMG') {
-        return;
+class Gallery { 
+    constructor(galleryItems, galleryParentEl) { 
+        this.galleryItems = galleryItems;
+        this.galleryParentEl = galleryParentEl;
     }
+
+    init() { 
+        const galleryEls = this.galleryItems.map(this.makeGalleryItem).join('');
+        this.galleryParentEl.insertAdjacentHTML('beforeend', galleryEls);
+        this.galleryParentEl.addEventListener('click', this.onPreviewImgClick.bind(this));
+    }
+
+    makeGalleryItem({ preview, original, description }) {
+        const itemEl = `<div class="gallery__item">
+                    <a class="gallery__link" href=${original}>
+                        <img
+                        class="gallery__image"
+                        src=${preview}
+                        data-source=${original}
+                        alt=${description}
+                        />
+                    </a>
+                </div>`;
+        return itemEl;
+    }
+
+    onPreviewImgClick(event) { 
+        if (event.target.nodeName !== 'IMG') {
+            return;
+        }
     
-    openOriginalImgModally(event);
+        this.openOriginalImgModally(event);
+    }
+
+    openOriginalImgModally(event) {
+        const original = event.target.dataset.source;
+        const description = event.target.alt;
+
+        event.preventDefault();
+        
+        this.modal = basicLightbox.create(
+            `<div class="modal">
+                <img
+                    src='${original}'
+                    alt='${description}'
+                />
+            </div>`,
+            {
+                onClose: () => window.removeEventListener('keydown', this.onEscKeyDown.bind(this))
+            },
+        );
+
+        this.modal.show(window.addEventListener('keydown', this.onEscKeyDown.bind(this)));
+    }
+
+    onEscKeyDown(event) {
+        if (event.code === 'Escape') {
+            this.modal.close();
+        }
+    }
 }
 
-function openOriginalImgModally(event) {
-    const original = event.target.dataset.source;
-    const description = event.target.alt;
-
-    event.preventDefault();
-    
-    const instance = basicLightbox.create(
-        `<div class="modal">
-            <img
-                src='${original}'
-                alt='${description}'
-            />
-        </div>`
-    )
-    instance.show();
-
-    window.addEventListener('keydown', onEscKeyDown);
-
-    // console.dir(instance);
-
-    onEscKeyDown() { }
-}
+new Gallery(galleryItems, galleryParentEl).init();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryParentEl = document.querySelector('.gallery');
+
+const galleryEls = galleryItems.map(makeGalleryItem).join('');
+galleryParentEl.insertAdjacentHTML('beforeend', galleryEls);
+galleryParentEl.addEventListener('click', onPreviewImgClick);
+
+const modal = new SimpleLightbox(
+                                    '.gallery a',
+                                    {
+                                        captionsData: 'alt',
+                                        captionDelay: 250,
+                                    }
+                                );
+
+function makeGalleryItem({ preview, original, description }) {
+    const itemEl = `<a class="gallery__item" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="Image ${description}" />
+                    </a>`;
+    return itemEl;
+}
+
+function onPreviewImgClick(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    event.modal.open();
+}
